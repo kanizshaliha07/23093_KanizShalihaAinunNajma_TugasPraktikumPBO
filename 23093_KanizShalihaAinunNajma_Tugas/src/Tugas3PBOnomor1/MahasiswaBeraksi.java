79c6976fa1f77562349bd39b562e1ca1f6317386
KANIZ SHALIHA AINUN NAJMA <147938663+kanizshaliha07@users.noreply.github.com>
2024-08-28 23:33:14 +0700

    Add files via upload

---
+++ 23093_KanizShalihaAinunNajma_Tugas/src/Tugas3PBOnomor1/MahasiswaBeraksi.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg23093_kanizshalihaainunnajma_tugas;
+
+/**
+ *
+ * @author LG
+ */
+public class MahasiswaBeraksi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Mahasiswa mahasiswa1 = new Mahasiswa();
+    
+    //Memanggil method-method dalam class mahasiswa
+    mahasiswa1.membaca();
+    mahasiswa1.nyontek();
+    mahasiswa1.modifikasi();
+    }
+    
+}
